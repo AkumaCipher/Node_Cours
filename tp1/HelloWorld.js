2cc9f4b0fa8227c16b2e35ae525d74712abc7873
AkumaCipher <petitmamass59@gmail.com>
2023-01-27 09:34:30 +0100

    TP1 avec les premiers gets

--- tp1/HelloWorld.js
+++ tp1/HelloWorld.js
@@ -1,9 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const error = require("./error.json");
+const app = express();
+const port = 4000;
 
 app.get('/massias', (req, res) => {
   res.send('Hello World!');
+})
+
+app.get('/welcome', (req, res) => {
+  res.send('Bienvenue sur le TP1 d\'architecture logicielle!');
+})
+
+app.get('/secret', (req, res) => {
+  res.status(401).send("Vous ne possédez pas les droits pour accéder à ma page secrète.");
+})
+
+app.get('/error', (req, res) => {
+  res.status(500).json(error);
+})
+
+app.get('/img', (req, res) => {
+  res.download("C:/Travail/Cours_BUT/Nodejs/Node_Cours/tp1/images/mario.png");
+})
+
+app.get('/redirectMe', (req, res) => {
+  res.redirect('https://www.iut-littoral.fr');
 })
 
 app.listen(port, () => {
